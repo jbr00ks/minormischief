6fed49351df2767c1ad07002d408dd066ee1df66
Josh Brooks <jdbrooks0516@gmail.com>
2021-12-07 21:36:14 -0600

    fix it

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react'
 import logo from './logo.svg';
 
-import { BroswerRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import About from './About.js'
 
 import './App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-        <div className="App">
-            <Route path="/about" component={About} />
-
-            <div className="navigation">
-                <img src={logo} className="App-logo" alt="logo" />
-                <div className="navigation-sub">
-                    <Link to="/about" className="item">About</Link>
-                </div>
-            </div>
-            <h1>Hello from Minor Mischief</h1>
-        </div>
-    </BroswerRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Route path="/about" component={About} />
+				<div className="navigation">
+					<img src={logo} className="App-logo" alt="logo" />
+					<div className="navigation-sub">
+						<Link to="/about" className="item">About</Link>
+					</div>
+				</div>
+				<h1>Hello from Minor Mischief</h1>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
