e7521f2cb2ec1e296133ab3ad3a731c133d60e36
Josh Brooks <jdbrooks0516@gmail.com>
2021-12-08 19:02:40 -0600

    new styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react'
-import logo from './logo.svg';
+import React from 'react';
+import About from './components/About.js'
+import Contact from './components/Contact.js'
+import Navbar from './components/Navbar.js'
+import Projects from './components/Projects.js'
 
-import { BrowserRouter, Route, Link } from 'react-router-dom'
-import About from './About.js'
-
-import './App.css';
-
-function App() {
+export default function App() {
 	return (
-		<BrowserRouter>
-			<div className="App">
-				<Route path="/about" component={About} />
-				<div className="navigation">
-					<img src={logo} className="App-logo" alt="logo" />
-					<div className="navigation-sub">
-						<Link to="/about" className="item">About</Link>
-					</div>
-				</div>
-				<h1>Hello from Minor Mischief</h1>
-			</div>
-		</BrowserRouter>
+		<main className="text-white bg-yellow-500 body-font">
+			<Navbar />
+			<Projects />
+			<About />
+			<Contact />
+		</main>
 	);
 }
-
-export default App;
